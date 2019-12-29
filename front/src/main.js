50e5edc2b6815a19b186d8cfafc30ec4d0b6d103
fengzhi <374136214@qq.com>
2019-12-29 20:31:18 +0800

    create blog vue

---
+++ front/src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Axios from 'axios'
+import Qs from 'qs'
+
+Axios.defaults.withCredentials = true
+Vue.prototype.qs = Qs
+Vue.prototype.$ajax = Axios
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
