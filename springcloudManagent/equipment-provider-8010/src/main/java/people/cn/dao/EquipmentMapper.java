2599d9aa2a97b085e67b41d06e9a1c43c42c5341
fengzhi <374136214@qq.com>
2020-07-06 22:41:00 +0800

    add equipmentMapper.xml

--- springcloudManagent/equipment-provider-8010/src/main/java/people/cn/dao/EquipmentMapper.java
+++ springcloudManagent/equipment-provider-8010/src/main/java/people/cn/dao/EquipmentMapper.java
@@ -13,47 +13,85 @@ import java.util.List;
  */
 @Mapper
 public interface EquipmentMapper {
-
+    /**
+     * 查询所有的设备
+     * @return
+     */
     List<Equipment> findAllEquipment();
 
-
-    Equipment findEquipmentById(String id);
+    /**
+     * 通过id查询设备
+     * @param id
+     * @return
+     */
+    Equipment findEquipmentById(@Param("id") String id);
 
     /**
      * 通过设备类型查找
      * @param gid
      * @return
      */
+    List<Equipment> findEquipmentByGroupId(@Param("gid") String gid);
 
-    List<Equipment> findEquipmentByGroupId(String gid);
-
+    /**
+     * 更新设备
+     * @param equipment
+     */
     void updateEquipment(Equipment equipment);
 
+    /**
+     * 添加设备
+     * @param equipment
+     */
     void saveEquipment(Equipment equipment);
 
+    /**
+     * 添加使用信息
+     * @param info
+     */
     void saveUseEquipment(UseEquipmentInfo info);
 
-    void stopUseEquipmentById(String ueid);
+    /**
+     * 停止使用某各设备
+     * @param ueid
+     */
+    void stopUseEquipmentById(@Param("ueid") String ueid);
 
+    /**
+     * 更新使用设备信息
+     * @param info
+     */
     void updateUseEquipment(UseEquipmentInfo info);
 
-
+    /**
+     * 获取全部的设备使用信息
+     * @return
+     */
     List<UseEquipmentInfo> findAllUseEquipment();
 
-
-    List<UseEquipmentInfo> findStatusUseEquipment(String status);
+    /**
+     * 获取正在使用的设备
+     * @param status
+     * @return
+     */
+    List<UseEquipmentInfo> findStatusUseEquipment(@Param("status") String status);
 
     /**
      * 分配设备后改变设备分配状态 0：正在使用
-     *
+     * @param eid
      */
-    void changeMetrics(String eid);
+    void changeMetrics(@Param("eid") String eid);
 
     /**
      * 分配设备后启用设备并改变涉笔分配状态 metrics 0 :用户正在使用 status 0 ：设备已启用
+     * @param eid
      */
-    void changeMetricsAndStatus(String eid);
+    void changeMetricsAndStatus(@Param("eid") String eid);
 
+    /**
+     * 获取全部的设备分类
+     * @return
+     */
     List<GroupDict> findEquipmentGroup();
 
     /**
@@ -70,9 +108,17 @@ public interface EquipmentMapper {
 
     /**
      * 通过模糊查找设备id
+     * @param code
+     * @return
      */
     List<Equipment> likeEquepment(String code);
 
+    /**
+     *
+     * @param brand
+     * @param code
+     * @return
+     */
     List<Equipment> findEquipmentByCodeAndBrand(String brand, String code);
 
     /**
